dccb2d90ea9618ca91d0286ca8f7bd4702f74bed
Andrey <126952251+Ozzzurk@users.noreply.github.com>
2023-04-14 19:35:41 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -39,3 +39,53 @@ if (isMobile.any()) {
 }else{
     document.body.classList.add('_pc');
 }
+
+
+//Меню для смартфонов
+const iconMenu = document.querySelector('.menu__icon');
+if(iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('lock');
+        iconMenu.classList.toggle('active');
+        menuBody.classList.toggle('active');
+    });
+}
+
+
+//Плавное появление
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        }
+    });
+}
+
+let options = {
+    threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
+
+
+
+//Кнопка отмотки вверх
+window.addEventListener('scroll', function () {
+    let scroll = document.querySelector('.upward');
+    scroll.classList.toggle("active", window.scrollY > 500);
+})
+
+const goToBtn = document.querySelector(".upward");
+
+goToBtn.addEventListener("click", scrollTop);
+
+function scrollTop() {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -75);
+        setTimeout(scrollTop, 0);
+    }
+}
